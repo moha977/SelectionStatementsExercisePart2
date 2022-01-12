fbae3c0228e3d461c5c9b05db8c40dbefd22b380
Mohammad ahma <ahmad.moh0812@icloud.com>
2022-01-12 16:39:52 -0500

    Add project files.

---
+++ SelectionStatementsExercisePart2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionStatementsExercisePart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your favorite school subject");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "Math":
+                    Console.WriteLine("Math is awesome");
+                    break;
+                case "History":
+                    Console.WriteLine("Very intresting");
+                    break;
+                case "English":
+                    Console.WriteLine("Welcome to America");
+                    break;
+                case "science":
+                    Console.WriteLine("Very tough");
+                    break;
+                case "coding":
+                    Console.WriteLine("C# is the best");
+                        break;
+                    default:
+                    Console.WriteLine("Try finding something you good at and like at the same time ");
+                    break;
+            }
+        }
+    }
+}
